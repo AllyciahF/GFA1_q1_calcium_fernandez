89fe2a070ce30c0007d576039e6a889b46f0475d
AllyciahF <143574154+AllyciahF@users.noreply.github.com>
2024-03-05 15:01:31 +0700

    Update 3.js

--- 3.js
+++ 3.js
@@ -1,7 +1,7 @@
 let text= "Do you want to share you're personal information OK or cancel.";
 if (confirm(text)==true)
 {var username= prompt("Please Enter Your Name:");
-var h = prompt("Pleaee Your Height (in inches): ");
+var h = prompt("Please Enter Your Height (in inches): ");
 var w=prompt ("Please Enter your Weight ( in kg)");
 var heigh=h/12;
 var weigh=(w*2.2);

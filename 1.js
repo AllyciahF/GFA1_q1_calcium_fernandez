7380bcef9c00e457b11b5a75b7f94d1ad6647d09
AllyciahF <143574154+AllyciahF@users.noreply.github.com>
2024-03-05 14:50:12 +0700

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,13 +1,12 @@
 
-var username= prompt("Please Enter Your Name: ");
-var h= prompt("Please Enter Your Height(in inches): ");
-var w= prompt("Please Enter Your Weight(in kg):");
-
-var heigh=h/12;
-var weigh=(w*2.2);
-var weight=weigh.toFixed(3);
-var height=heigh.toFixed(1)
-
+        var username= prompt("Please Enter Your Name: ");
+        var h= prompt("Please Enter Your Height (in inches): ");
+        var w= prompt("Please Enter Your Weight (in Pounds): ");
+        var heigh=h/12;
+        var weigh=(w*2.2);
+        var weight=weigh.toFixed(3);
+        var height=heigh.toFixed(1);
+        alert("Nickname: " +username+ "\n Height: " +height+ "\n Weight: " +weight+ "lbs");
 
 
 
